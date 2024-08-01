7629bb3c0bedba44cf03964048b048efe10917c0
Luke <2433042861@qq.com>
2024-08-01 22:46:59 +0800

    token的注入
    此次提交实现了用户信息的获取功能，并确保了每次请求都会携带token，同时也统一了配置格式。

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,4 +1,11 @@
+import { requst } from "@/utils"
+import { useEffect } from "react"
 const Layout = () => {
+
+    useEffect(() => {
+        requst.get('/user/profile')
+    }, [])
+
     return <div>a layout</div>
 }
 
