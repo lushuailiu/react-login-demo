be29479f70f59cd0b5f84af50de15bbcd6215884
Luke <2433042861@qq.com>
2024-08-01 12:19:45 +0800

    feat(Login page): implement login form with Ant Design components
    在登录页面中，实现了使用Ant Design的表单、按钮、卡片和输入组件的登录表单。还包括了登录logo的导入和对应的样式文件引用。此次改动旨在提升用户界面的交互性和视觉体验，同时遵循组件化开发的原则。
    
    修改了jsconfig.json以添加路径别名配置，提高了项目代码的可维护性和可读性。同时，对Login组件的index.js进行了大范围的修改，包括导入必要的Ant Design组件，编写登录表单的结构和样式，以及替换原有的简单返回文本为登录表单的渲染逻辑。
    
    BREAKING CHANGE: 本次更新大幅修改了Login组件的结构，如果其他部分有依赖于Login组件的直接渲染输出，需要相应地进行调整以适配新的表单实现。

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,26 @@
+import { Button, Card, Form, Input } from 'antd'
+import logo from '@/assets/logo.png'
+import '@/pages/Login/index.scss'
 const Login = () => {
-    return <div> this is Login </div>
+    return (
+        <div className="login">
+            <Card className="login-container">
+                <img className="login-logo" src={ logo } alt="" />
+                {/* 登录表单 */}
+                <Form>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入手机号"></Input>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input size="large" placeholder="请输入验证码"></Input>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" size="large" block>登录</Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </div>
+    )
 }
 
 export default Login
