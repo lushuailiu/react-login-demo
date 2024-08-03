205bc8b074f6c35c014daf79f14f05140b3c0c4a
Luke <2433042861@qq.com>
2024-08-03 16:53:36 +0800

    ``` store/modules/user.js: 完善用户信息获取功能
    - 添加 `setUserInfo` reducer 处理获取到的用户信息。
    - 实现 `fetchUserInfo` 异步操作，用于从 API 获取用户信息并提交给 reducer。
    - 在 `GeekLayout` 组件中调用 `fetchUserInfo` 以获取用户信息。
    - 其他代码优化，例如注释的清理和整理。
    ```

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -9,7 +9,7 @@ import './index.scss'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-// import { fetchUserInfo, clearUserInfo } from '@/store/modules/user'
+import { fetchUserInfo, clearUserInfo } from '@/store/modules/user'
 
 const { Header, Sider } = Layout
 
@@ -42,14 +42,15 @@ const GeekLayout = () => {
   // 反向高亮
   // 1. 获取当前路由路径
   const location = useLocation()
+  console.log("获取当前路由路径")
   console.log(location.pathname)
   const selectedkey = location.pathname
 
   // 触发个人用户信息action
   const dispatch = useDispatch()
-//   useEffect(() => {
-//     dispatch(fetchUserInfo())
-//   }, [dispatch])
+  useEffect(() => {
+    dispatch(fetchUserInfo())
+  }, [dispatch])
 
   // 退出登录确认回调
   const onConfirm = () => {
@@ -57,14 +58,14 @@ const GeekLayout = () => {
     // dispatch(clearUserInfo())
     navigate('/login')
   }
-
-//   const name = useSelector(state => state.user.userInfo.name)
+  // 获取用户信息
+  const name = useSelector(state => state.user.userInfo.name)
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
         <div className="user-info">
-          {/* <span className="user-name">{name}</span> */}
+          <span className="user-name">{name}</span>
           <span className="user-logout">
             <Popconfirm title="是否确认退出？" okText="退出" cancelText="取消" onConfirm={onConfirm}>
               <LogoutOutlined /> 退出
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -5,7 +5,8 @@ const userState = createSlice({
     name: 'user',
     //数据状态
     initialState: {
-        token: getToken() || ''
+        token: getToken() || '',
+        userInfo: {}
     },
     //同步修改方法
     reducers: {
@@ -13,12 +14,15 @@ const userState = createSlice({
             state.token = action.payload
             //localStore 存储token
             _setToken(action.payload)
+        },
+        setUserInfo(state, action) {
+            state.userInfo = action.payload
         }
     }
 })
 
 //解构出actionCreater
-const { setToken } = userState.actions
+const { setToken, setUserInfo } = userState.actions
 
 //获取 reducer函数
 const userReducer = userState.reducer
@@ -33,6 +37,17 @@ const fetchLogin = (loginForm) => {
     }
 }
 
-export { fetchLogin, setToken }
+//获取个人用户信息 异步
+const fetchUserInfo = () => {
+    return async (dispatch) => {
+        //1.发送异步请求
+        const res = await requst.get('/user/profile')
+        //2.提交同步action 
+        console.log("res", res)
+        dispatch(setUserInfo(res.data))
+    }
+}
+
+export { fetchLogin, setToken, fetchUserInfo }
 
 export default userReducer
